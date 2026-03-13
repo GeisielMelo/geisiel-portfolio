8104f221070b34bc4cce009dbc8f2366405c7e5a
Geisiel Melo <geisiel.nascimento@gmail.com>
2026-03-13 12:24:21 -0300

    feat: save

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -5,6 +5,10 @@ export const routing = defineRouting({
   locales: ['en', 'pt'],
   defaultLocale: 'en',
   localePrefix: 'as-needed',
+  localeCookie: {
+    name: 'NEXT_LOCALE',
+    maxAge: 60 * 60 * 24 * 365,
+  },
 })
 
 export const { Link, redirect, usePathname, useRouter, getPathname } = createNavigation(routing)
